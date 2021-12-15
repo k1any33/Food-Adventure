fe8044167386505d3cc9922d2a456476f28c2cff
Zack Ng <ngzhicai98@gmail.com>
2021-12-14 20:40:59 -0800

    Add static keyword to class static vars

--- client/src/api/FoodApi.js
+++ client/src/api/FoodApi.js
@@ -3,15 +3,15 @@ import axios from './index';
 export class FoodApi {
   static ENDPOINT = '/food-posts';
 
-  fetchFoodPosts = async (page) => axios.get(`${FoodApi.ENDPOINT}?page=${page}`)
+  static fetchFoodPosts = async (page) => axios.get(`${FoodApi.ENDPOINT}?page=${page}`)
 
-  fetchFoodPostsBySearch = async (searchQuery) => axios.get(`${FoodApi.ENDPOINT}/search?searchQuery=${searchQuery.search || 'none'}`)
+  static fetchFoodPostsBySearch = async (searchQuery) => axios.get(`${FoodApi.ENDPOINT}/search?searchQuery=${searchQuery.search || 'none'}`)
 
-  createFoodPost = async (foodPost) => axios.post(`'${FoodApi.ENDPOINT}'`, foodPost);
+  static createFoodPost = async (foodPost) => axios.post(`'${FoodApi.ENDPOINT}'`, foodPost);
 
-  updateFoodPost = async (id, foodPost) => axios.patch(`${FoodApi.ENDPOINT}/${id}`, foodPost);
+  static updateFoodPost = async (id, foodPost) => axios.patch(`${FoodApi.ENDPOINT}/${id}`, foodPost);
 
-  deleteFoodPost = async (id, foodPost) => axios.delete(`${FoodApi.ENDPOINT}/${id}`, foodPost);
+  static deleteFoodPost = async (id, foodPost) => axios.delete(`${FoodApi.ENDPOINT}/${id}`, foodPost);
 
-  likeFoodPost = async (id, foodPost) => axios.patch(`${FoodApi.ENDPOINT}/${id}/likeFoodPost`, foodPost);
+  static likeFoodPost = async (id, foodPost) => axios.patch(`${FoodApi.ENDPOINT}/${id}/likeFoodPost`, foodPost);
 }
--- client/src/api/UserApi.js
+++ client/src/api/UserApi.js
@@ -3,6 +3,7 @@ import axios from './index';
 export class UserApi {
   static ENDPOINT = '/user';
 
-  login = (userData) => axios.post(`${UserApi.ENDPOINT}/login`, userData);
-  register = (userData) => axios.post(`${UserApi.ENDPOINT}/register`, userData);
+  static login = (userData) => axios.post(`${UserApi.ENDPOINT}/login`, userData);
+
+  static register = (userData) => axios.post(`${UserApi.ENDPOINT}/register`, userData);
 }
