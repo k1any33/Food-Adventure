44a01445b6749fd73cc26fadf44d92c1648f2435
Zack Ng <ngzhicai98@gmail.com>
2021-12-14 20:03:10 -0800

    Fix error

--- client/src/api/FoodApi.js
+++ client/src/api/FoodApi.js
@@ -3,24 +3,15 @@ import axios from './index';
 export class FoodApi {
   static ENDPOINT = '/food-posts';
 
-  async fetchFoodPosts(page) {
-    axios.get(`${FoodApi.ENDPOINT}?page=${page}`);
-  }
-  async fetchFoodPostsBySearch(searchQuery) {
-    axios.get(
-      `${FoodApi.ENDPOINT}/search?searchQuery=${searchQuery.search || 'none'}`
-    );
-  }
-  async createFoodPost(newFoodPost) {
-    axios.post(`'${FoodApi.ENDPOINT}'`, newFoodPost);
-  }
-  async updateFoodPost(id, updatedFoodPost) {
-    axios.patch(`${FoodApi.ENDPOINT}/${id}`, updatedFoodPost);
-  }
-  async deleteFoodPost(id) {
-    axios.delete(`${FoodApi.ENDPOINT}/${id}`, deleteFoodPost);
-  }
-  async likeFoodPost(id) {
-    axios.patch(`${FoodApi.ENDPOINT}/${id}/likeFoodPost`, likeFoodPost);
-  }
+  fetchFoodPosts = async (page) => axios.get(`${FoodApi.ENDPOINT}?page=${page}`)
+
+  fetchFoodPostsBySearch = async (searchQuery) => axios.get(`${FoodApi.ENDPOINT}/search?searchQuery=${searchQuery.search || 'none'}`)
+
+  createFoodPost = async (foodPost) => axios.post(`'${FoodApi.ENDPOINT}'`, foodPost);
+
+  updateFoodPost = async (id, foodPost) => axios.patch(`${FoodApi.ENDPOINT}/${id}`, foodPost);
+
+  deleteFoodPost = async (id, foodPost) => axios.delete(`${FoodApi.ENDPOINT}/${id}`, foodPost);
+
+  likeFoodPost = async (id, foodPost) => axios.patch(`${FoodApi.ENDPOINT}/${id}/likeFoodPost`, foodPost);
 }
